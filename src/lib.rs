2e6b216ea1e2f39c147fa06648c1ddb6c2afcb20
Burak Emir <burak.emir@gmail.com>
2022-06-29 23:18:15 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+use std::vec::Vec;
+
+pub enum GaussEliminationOption {
+    JustEchelon,
+    PrepareReduce
+}
+
+// matrix is represented as vector of rows, a row being a vector of columns.
+pub fn gauss_elimination(matrix: &mut Vec<Vec<f32>>, option: GaussEliminationOption) {
+    fn find_pivot(matrix: &mut Vec<Vec<f32>>, d: usize) -> Option<usize> {
+        return (d..matrix.len()).find(|&i| matrix[i][d] != 0f32);
+    }
+    let nrows = matrix.len();
+    for c in 0..nrows {
+        match find_pivot(matrix, c) {
+            None => {}
+            Some(i) => {
+                for row in i + 1..nrows {
+                    let factor = matrix[row][c] / matrix[i][c];
+                    for col in c..matrix[row].len() {
+                        matrix[row][col] -= factor * matrix[i][col]
+                    }
+                }
+                // Move pivot to row c, in order to get a "real" echelon form.
+                if c != i {
+                    matrix.swap(i, c);
+                }
+                if matches!(option, GaussEliminationOption::PrepareReduce) {
+                    // normalize the pivot to 1.0
+                    let factor = 1.0 / matrix[c][c];
+                    for col in c..matrix[c].len() {
+                        matrix[c][col] *= factor
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn gauss_jordan_elimination(matrix: &mut Vec<Vec<f32>>) {
+    gauss_elimination(matrix, GaussEliminationOption::PrepareReduce);
+    let nrows = matrix.len();
+
+    fn find_pivot(matrix: &mut Vec<Vec<f32>>, d: usize) -> Option<usize> {
+        return (d..matrix.len()).find(|&i| matrix[d][i] != 0f32);
+    }
+
+    for d in (1..nrows).rev() {
+        match find_pivot(matrix, d) {
+            None => {}
+            Some(i) => {
+                for row in (0..d).rev() {
+                    let factor = matrix[row][i];
+                    for col in d..matrix[i].len() {
+                        matrix[row][col] -= factor * matrix[d][col]
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_gauss_elimination_echelon() {
+        let mut matrix = vec![
+            vec![1.0, 2.0, 1.0, 10.0],
+            vec![2.0, 3.0, 2.0, 12.0],
+            vec![3.0, 1.0, 4.0, 11.0],
+        ];
+        gauss_elimination(&mut matrix, GaussEliminationOption::JustEchelon);
+        assert_eq!(
+            matrix,
+            vec![
+                vec![1.0, 2.0, 1.0, 10.0],
+                vec![0.0, -1.0, 0.0, -8.0],
+                vec![0.0, 0.0, 1.0, 21.0],
+            ]
+        );
+    }
+
+    #[test]
+    fn test_gauss_elimination_reduce() {
+        let mut matrix = vec![
+            vec![1.0, 2.0, 1.0, 10.0],
+            vec![2.0, 3.0, 2.0, 12.0],
+            vec![3.0, 1.0, 4.0, 11.0],
+        ];
+        gauss_elimination(&mut matrix, GaussEliminationOption::PrepareReduce);
+        assert_eq!(
+            matrix,
+            vec![
+                vec![1.0, 2.0, 1.0, 10.0],
+                vec![0.0, 1.0, 0.0, 8.0],
+                vec![0.0, 0.0, 1.0, 21.0],
+            ]
+        );
+    }
+
+    #[test]
+    fn test_gauss_jordan_elimination() {
+        let mut matrix = vec![
+            vec![1.0, 2.0, 1.0, 10.0],
+            vec![2.0, 3.0, 2.0, 12.0],
+            vec![3.0, 1.0, 4.0, 11.0],
+        ];
+        gauss_jordan_elimination(&mut matrix);
+        assert_eq!(
+            matrix,
+            vec![
+                vec![1.0, 0.0, 0.0, -27.0],
+                vec![0.0, 1.0, 0.0, 8.0],
+                vec![0.0, 0.0, 1.0, 21.0],
+            ]
+        );
+    }
+
+    #[test]
+    fn test_gauss_elimination_zero() {
+        let mut matrix = vec![
+            vec![1.0, 2.0, 1.0, 10.0],
+            vec![0.0, 0.0, 2.0, 12.0],
+            vec![3.0, 1.0, 4.0, 11.0],
+        ];
+        gauss_elimination(&mut matrix, GaussEliminationOption::JustEchelon);
+        assert_eq!(
+            matrix,
+            vec![
+                vec![1.0, 2.0, 1.0, 10.0],
+                vec![0.0, -5.0, 1.0, -19.0],
+                vec![0.0, 0.0, 2.0, 12.0],
+            ]
+        );
+
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
